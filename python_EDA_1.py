d3ec31b14cc50590a1478545896d840c034dd4c8
OliverDuncan <60114296+OliverDuncan@users.noreply.github.com>
2020-01-20 13:08:16 -0700

    Update python_EDA_1.py

--- python_EDA_1.py
+++ python_EDA_1.py
@@ -1,7 +1,9 @@
 #########################################################################################
 #   This training will showcase exploratory data analysis methods in Python using our
 #   HEAL lab projects as a context.
-#     Coded by Jeff Swigert
-#     Last Updated: 1/17/2020
+#     Coded by Jeff Swigert and Oliver Duncan
+#     Last Updated: 1/20/2020
 #     Based on material covered in EDA in Python datacamp.com course
 #########################################################################################
+
+# Oliver Duncan cool code snippets
