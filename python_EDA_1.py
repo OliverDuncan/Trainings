82dbda784dfdb810f2cc927d3ad535e56e7fbcde
OliverDuncan <60114296+OliverDuncan@users.noreply.github.com>
2020-01-20 19:09:41 -0700

    Update python_EDA_1.py

--- python_EDA_1.py
+++ python_EDA_1.py
@@ -1,7 +1,22 @@
 #########################################################################################
 #   This training will showcase exploratory data analysis methods in Python using our
 #   HEAL lab projects as a context.
-#     Coded by Jeff Swigert
-#     Last Updated: 1/17/2020
+#     Coded by Jeff Swigert and Oliver Duncan
+#     Last Updated: 1/20/2020
 #     Based on material covered in EDA in Python datacamp.com course
 #########################################################################################
+
+# Cool snippets of code found by Oliver
+
+import numpy as np
+
+x = [4 , 9 , 6, 3, 1]
+y = np.array(x)
+
+high = y > 5
+y[high]
+
+print(high)
+print(y[high])
+
+# gk_heights = np_heights[np_positions == 'GK']
